7bc35fce144789cb003a05f89f42a60c130f080e
Grahn Cooledge <gcooled29@gmail.com>
2018-11-15 21:31:17 -0500

    Minor Server Migration Corrections

--- server-client.js
+++ server-client.js
@@ -1,3 +1,12 @@
-const args = ['start'];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
+const spawn = require('child_process').spawn;
+
+// const process = [
+//   { args: ['run server'], opts: { stdio: 'inherit', cwd: '.', shell: true } },
+//   { args: ['run client'], opts: { stdio: 'inherit', cwd: '.', shell: true } }
+// ];
+
+// process.forEach(element => spawn('npm', element.args, element.opts));
+
+const args = ['start', 'client'];
+const opts = { stdio: 'inherit', cwd: '.', shell: true };
+spawn('npm', args, opts);
